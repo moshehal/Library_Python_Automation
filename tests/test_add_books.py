935f4fca0da1690dab0d9238ba249b31f5970edf
Moshe <moshenav6365@gmail.com>
2025-03-16 22:19:40 +0200

    - add pre condition to test. - add verifications to test

--- tests/test_add_books.py
+++ tests/test_add_books.py
@@ -3,7 +3,12 @@ from api.library_api_calls import add_book, get_books
 
 def test_add_a_book():
     add_a_book_response = add_book("The Great Gatsby", "F. Scott Fitzgerald")
+    assert add_a_book_response.title == "The Great Gatsby"
+    assert add_a_book_response.author == "F. Scott Fitzgerald"
+    assert add_a_book_response.is_borrowed == False
+
     get_books_response = get_books()
+    assert add_a_book_response.id == get_books_response[-1].id
     assert get_books_response[-1].title == "The Great Gatsby"
     assert get_books_response[-1].author == "F. Scott Fitzgerald"
 
--- tests/test_get_books.py
+++ tests/test_get_books.py
@@ -1,6 +1,12 @@
 import pytest
 from api.library_api_calls import get_books, delete_book
 
+@pytest.fixture
+def setup_no_books_on_library():
+    get_books_response = get_books()
+    for book in get_books_response:
+        delete_book(book.id)
+
 def test_get_books(setup_books_on_library):
     books = get_books()
     assert len(books) > 0
@@ -9,10 +15,7 @@ def test_get_books(setup_books_on_library):
     assert isinstance(books[0].author, str)
     assert isinstance(books[0].is_borrowed, bool)
 
-def test_get_books_empty(setup_books_on_library):
-    get_books_response = get_books()
-    for book in get_books_response:
-        delete_book(book.id)
+def test_get_books_empty(setup_no_books_on_library):
     books = get_books()
     print (books)
     assert len(books) == 0
